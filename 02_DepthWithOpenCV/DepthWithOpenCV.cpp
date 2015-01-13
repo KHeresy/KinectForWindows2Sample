abfe42bc55b2f69e468800d1fd22b258b0107c89
Heresy <kheresy@gmail.com>
2015-01-13 10:04:33 +0800

    remove variable for test

--- 02_DepthWithOpenCV/DepthWithOpenCV.cpp
+++ 02_DepthWithOpenCV/DepthWithOpenCV.cpp
@@ -79,7 +79,7 @@ int main(int argc, char** argv)
 						if (pFrameReader->AcquireLatestFrame(&pFrame) == S_OK)
 						{
 							// 4c. copy the depth map to image
-							auto res = pFrame->CopyFrameDataToArray(iWidth * iHeight, reinterpret_cast<UINT16*>(mDepthImg.data));
+							pFrame->CopyFrameDataToArray(iWidth * iHeight, reinterpret_cast<UINT16*>(mDepthImg.data));
 
 							// 4d. convert from 16bit to 8bit
 							mDepthImg.convertTo(mImg8bit, CV_8U, 255.0f / uDepthMax );
