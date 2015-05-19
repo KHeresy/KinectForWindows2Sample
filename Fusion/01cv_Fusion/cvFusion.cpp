f5b8617b2c27bff14628547b038887c1543884a6
Heresy <kheresy@gmail.com>
2015-05-19 15:29:26 +0800

    simplify matrix operation

--- Fusion/01cv_Fusion/cvFusion.cpp
+++ Fusion/01cv_Fusion/cvFusion.cpp
@@ -17,19 +17,10 @@
 
 using namespace std;
 
-void SetIdentityMatrix(Matrix4 &mat)
-{
-	mat.M11 = 1; mat.M12 = 0; mat.M13 = 0; mat.M14 = 0;
-	mat.M21 = 0; mat.M22 = 1; mat.M23 = 0; mat.M24 = 0;
-	mat.M31 = 0; mat.M32 = 0; mat.M33 = 1; mat.M34 = 0;
-	mat.M41 = 0; mat.M42 = 0; mat.M43 = 0; mat.M44 = 1;
-}
-
-void ResetReconstruction(INuiFusionReconstruction* pReconstruction, Matrix4* pMat)
+void ResetReconstruction(INuiFusionReconstruction* pReconstruction)
 {
 	std::cout << "Reset Reconstruction" << std::endl;
-	SetIdentityMatrix(*pMat);
-	pReconstruction->ResetReconstruction(pMat, nullptr);
+	pReconstruction->ResetReconstruction(nullptr, nullptr);
 }
 
 bool OutputSTL(INuiFusionMesh* pMesh)
@@ -141,18 +132,14 @@ int main(int argc, char** argv)
 	#pragma region Kinect Fusion Code
 	// Volume size and resolution configuration
 	NUI_FUSION_RECONSTRUCTION_PARAMETERS mResolution;
-	mResolution.voxelsPerMeter = 256;
-	mResolution.voxelCountX = 512;
-	mResolution.voxelCountY = 512;
-	mResolution.voxelCountZ = 512;
-
-	// default matrix
-	Matrix4 worldToCameraTransform;
-	SetIdentityMatrix(worldToCameraTransform);
+	mResolution.voxelsPerMeter = 64;
+	mResolution.voxelCountX = 256;
+	mResolution.voxelCountY = 256;
+	mResolution.voxelCountZ = 256;
 
 	// create INuiFusionReconstruction
 	INuiFusionReconstruction* pReconstruction = nullptr;
-	if (NuiFusionCreateReconstruction(&mResolution, NUI_FUSION_RECONSTRUCTION_PROCESSOR_TYPE_AMP, -1, &worldToCameraTransform, &pReconstruction) != S_OK)
+	if (NuiFusionCreateReconstruction(&mResolution, NUI_FUSION_RECONSTRUCTION_PROCESSOR_TYPE_AMP, -1, nullptr, &pReconstruction) != S_OK)
 	{
 		std::cerr << "Kinect Fusion object create failed" << std::endl;
 		return -1;
@@ -209,6 +196,7 @@ int main(int argc, char** argv)
 			fDepthMax = NUI_FUSION_DEFAULT_MAXIMUM_DEPTH;
 	USHORT	uIterationCount = NUI_FUSION_DEFAULT_ALIGN_ITERATION_COUNT,
 			uIntegrationWeight = NUI_FUSION_DEFAULT_INTEGRATION_WEIGHT;
+	Matrix4 worldToCameraTransform;
 
 	while (true)
 	{
@@ -236,7 +224,7 @@ int main(int argc, char** argv)
 							if (errorCount >= 100)
 							{
 								errorCount = 0;
-								ResetReconstruction(pReconstruction, &worldToCameraTransform);
+								ResetReconstruction(pReconstruction);
 							}
 						}
 
@@ -281,7 +269,7 @@ int main(int argc, char** argv)
 		}
 		else if (key == 'r')
 		{
-			ResetReconstruction(pReconstruction, &worldToCameraTransform);
+			ResetReconstruction(pReconstruction);
 		}
 		else if (key == 'o')
 		{
--- Fusion/02cv_ColorFusion/cvColorFusion.cpp
+++ Fusion/02cv_ColorFusion/cvColorFusion.cpp
@@ -17,19 +17,10 @@
 
 using namespace std;
 
-void SetIdentityMatrix(Matrix4 &mat)
-{
-	mat.M11 = 1; mat.M12 = 0; mat.M13 = 0; mat.M14 = 0;
-	mat.M21 = 0; mat.M22 = 1; mat.M23 = 0; mat.M24 = 0;
-	mat.M31 = 0; mat.M32 = 0; mat.M33 = 1; mat.M34 = 0;
-	mat.M41 = 0; mat.M42 = 0; mat.M43 = 0; mat.M44 = 1;
-}
-
-void ResetReconstruction(INuiFusionColorReconstruction* pReconstruction, Matrix4* pMat)
+void ResetReconstruction(INuiFusionColorReconstruction* pReconstruction)
 {
 	std::cout << "Reset Reconstruction" << std::endl;
-	SetIdentityMatrix(*pMat);
-	pReconstruction->ResetReconstruction(pMat, nullptr);
+	pReconstruction->ResetReconstruction(nullptr , nullptr);
 }
 
 bool OutputOBJ(INuiFusionColorMesh* pMesh)
@@ -199,13 +190,9 @@ int main(int argc, char** argv)
 	mResolution.voxelCountY = 256;
 	mResolution.voxelCountZ = 256;
 
-	// default matrix
-	Matrix4 worldToCameraTransform;
-	SetIdentityMatrix(worldToCameraTransform);
-
 	// create INuiFusionReconstruction
 	INuiFusionColorReconstruction* pReconstruction = nullptr;
-	if (NuiFusionCreateColorReconstruction(&mResolution, NUI_FUSION_RECONSTRUCTION_PROCESSOR_TYPE_AMP, -1, &worldToCameraTransform, &pReconstruction) != S_OK)
+	if (NuiFusionCreateColorReconstruction(&mResolution, NUI_FUSION_RECONSTRUCTION_PROCESSOR_TYPE_AMP, -1, nullptr, &pReconstruction) != S_OK)
 	{
 		std::cerr << "Kinect Fusion object create failed" << std::endl;
 		return -1;
@@ -271,6 +258,7 @@ int main(int argc, char** argv)
 	USHORT	uIterationCount = NUI_FUSION_DEFAULT_ALIGN_ITERATION_COUNT,
 			uIntegrationWeight = NUI_FUSION_DEFAULT_INTEGRATION_WEIGHT;
 	float	fColorInregrationAngle = NUI_FUSION_DEFAULT_COLOR_INTEGRATION_OF_ALL_ANGLES;
+	Matrix4 worldToCameraTransform;
 
 	ColorSpacePoint* pColorPoint = new ColorSpacePoint[uDepthPointNum];
 	BYTE* pColorData = new BYTE[iColorWidth * iColorHeight * 4];
@@ -336,7 +324,7 @@ int main(int argc, char** argv)
 			if (errorCount >= 100)
 			{
 				errorCount = 0;
-				ResetReconstruction(pReconstruction, &worldToCameraTransform);
+				ResetReconstruction(pReconstruction);
 			}
 		}
 
@@ -374,7 +362,7 @@ int main(int argc, char** argv)
 		}
 		else if (key == 'r')
 		{
-			ResetReconstruction(pReconstruction, &worldToCameraTransform);
+			ResetReconstruction(pReconstruction);
 		}
 		else if (key == 'o')
 		{
